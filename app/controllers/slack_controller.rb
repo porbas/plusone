fbc0bb33cad9cdf00b8e37711bc3d74c5de2a3bd
Robert Pankowecki <robert.pankowecki@gmail.com>
2014-11-06 13:01:12 +0100

    2 more steps

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -14,29 +14,15 @@ class SlackController < ApplicationController
     sender.save!
 
     recipient_name.present? or raise MissingRecipient
-    if recipient_name == "!stats"
-      team = Team.find_or_initialize_by(slack_team_id: params[:team_id])
-      team.slack_team_domain = params[:team_domain]
-      team.save!
+    recipient = team.team_members.find_or_initialize_by(slack_user_name: recipient_name)
+    recipient.save!
 
-      msg = team.team_members.sort_by{|tm| tm.points }.reverse.map{|tm| "#{tm.slack_user_name}: #{tm.points}"}.join(", ")
+    raise CannotPlusOneYourself if sender == recipient
+    recipient.increment!(:points)
 
-      respond_to do |format|
-        format.json do
-          render json: {text: msg}
-        end
-      end
-    else
-      recipient = team.team_members.find_or_initialize_by(slack_user_name: recipient_name)
-      recipient.save!
-
-      raise CannotPlusOneYourself if sender == recipient
-      recipient.increment!(:points)
-
-      respond_to do |format|
-        format.json do
-          render json: {text: "#{sender.slack_user_name}(#{sender.points}) gave +1 for #{recipient.slack_user_name}(#{recipient.points})"}
-        end
+    respond_to do |format|
+      format.json do
+        render json: {text: "#{sender.slack_user_name}(#{sender.points}) gave +1 for #{recipient.slack_user_name}(#{recipient.points})"}
       end
     end
   rescue CannotPlusOneYourself
@@ -45,15 +31,41 @@ class SlackController < ApplicationController
         render json: {text: "Nope... not gonna happen."}
       end
     end
-  rescue MissingRecipient
+  end
+
+  def stats
+    team = Team.find_or_initialize_by(slack_team_id: params[:team_id])
+    team.slack_team_domain = params[:team_domain]
+    team.save!
+
+    sender = team.team_members.find_or_initialize_by(slack_user_name: params[:user_name])
+    sender.slack_user_id = params[:user_id]
+    sender.save!
+
+    msg = team.team_members.sort_by{|tm| tm.points }.reverse.map{|tm| "#{tm.slack_user_name}: #{tm.points}"}.join(", ")
+
+    respond_to do |format|
+      format.json do
+        render json: {text: msg}
+      end
+    end
+  end
+
+  def empty
+    team = Team.find_or_initialize_by(slack_team_id: params[:team_id])
+    team.slack_team_domain = params[:team_domain]
+    team.save!
+
+    sender = team.team_members.find_or_initialize_by(slack_user_name: params[:user_name])
+    sender.slack_user_id = params[:user_id]
+    sender.save!
+
     respond_to do |format|
       format.json do
         render json: {text: "?"}
       end
     end
   end
-
-
 
   private
 

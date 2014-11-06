88dbb0b929b44e128f8b137b2d10d36d0743096d
Robert Pankowecki <robert.pankowecki@gmail.com>
2014-11-06 12:13:40 +0100

    Constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+
+  class StatsConstraint
+    def matches?(request)
+      MessageParser.new(
+        request.request_parameters['text'],
+        request.request_parameters['trigger_word']
+      ).recipient_name == "!stats"
+    end
+  end
+
+  class EmptyConstraint
+    def matches?(request)
+      MessageParser.new(
+        request.request_parameters['text'],
+        request.request_parameters['trigger_word']
+      ).recipient_name.empty?
+    end
+  end
+
   post "/slack" => "slack#create"
 
   root "slack#index"
